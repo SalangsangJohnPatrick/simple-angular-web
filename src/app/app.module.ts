279257be924170da29c215f863faf430ef859cac
John Patrick Salangsang <salangsangjpmoreno@gmail.com>
2024-12-13 22:28:22 +0800

    Implements lazy loading
    Used an alternative way in routing the Home and Contact page from the App component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
-    ContactPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
