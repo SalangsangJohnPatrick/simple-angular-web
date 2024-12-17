3e84a294c32d3e69fae53b207e8cbf1e1bf9930a
John Patrick Salangsang <salangsangjpmoreno@gmail.com>
2024-12-17 17:44:51 +0800

    Updated the contact form

--- src/app/sent-page/sent-page.component.ts
+++ src/app/sent-page/sent-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../services/contact.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-sent-page',
@@ -8,8 +9,24 @@ import { ContactService } from '../services/contact.service';
 })
 export class SentPageComponent implements OnInit {
 
-  // Expose messages as an observable
-  mails$ = this.contactService.mails$;
+  // Truncate helper function
+  private truncateText(text: string, maxLength: number): string {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + '...';
+    }
+    return text;
+  }
+
+  // Use pipe operator to transform data with RxJS
+  mails$ = this.contactService.mails$.pipe(
+    map(mails =>
+      mails.map(mail => ({
+        ...mail,
+        subject: this.truncateText(mail.subject, 30),  // Truncate subject to 30 characters
+        message: this.truncateText(mail.message, 50)   // Truncate message to 50 characters
+      }))
+    )
+  );
 
   constructor(private contactService: ContactService) {}
 
